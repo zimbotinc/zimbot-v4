a8dd63e5282634bc48ac7602b65e170cb9bb5b2b
zimbotinc <106673922+zimbotinc@users.noreply.github.com>
2022-10-02 14:38:08 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
+global.owner = ['27634090203','918188019676'] //owner number +27634090203
 global.wame = ['27634090203'] 
 global.pemilik = ['27634090203'] //owner number +27634090203
 global.premium = ['27634090203'] //premium number +27634090203
